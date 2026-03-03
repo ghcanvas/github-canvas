551aa6193db4612b442644985a10c5dfcf1ad6d6
Serge Urakhchin <urakhchin@gmail.com>
2026-03-03 02:16:46 -0600

    first github commit

--- src/app/services/main-service.ts
+++ src/app/services/main-service.ts
@@ -13,7 +13,7 @@ export interface User {
 export class MainService {
   private readonly usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  //test
+  //test2
 
   // state holder
   private readonly usersSubject = new BehaviorSubject<User[]>([]);
